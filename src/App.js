f45e90e3ac2f682da56975e0565d738127adf759
Michel <micheldcalil@hotmail.com>
2021-05-13 00:34:20 -0300

    Terminando o Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import "./App.css";
 import Card from "./Card";
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <Grid container>
-      <Grid item xs={6}>
+    <Grid
+      container
+      spacing={3}
+      className={classes.gridContainer}
+      justify="center"
+    >
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
         <Card />
       </Grid>
     </Grid>
