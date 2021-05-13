a6e2a9f9d4424e2a2ba1732beb2d2568cf959c38
Michel <micheldcalil@hotmail.com>
2021-05-13 01:07:09 -0300

    Lendo as infos da API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Card from "./Card";
+import Card from "@material-ui/core/Card";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -12,6 +15,23 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setPosts(result);
+        },
+
+        (error) => {
+          console.error(error);
+        }
+      );
+  }, []);
+
   return (
     <Grid
       container
@@ -20,31 +40,10 @@ function App() {
       justify="center"
     >
       <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card />
+        {posts.map((post, index) => (
+          // <h2 key={post.id}>{JSON.stringify(post)}</h2>
+          <Card key={post.id}>{post.body}</Card>
+        ))}
       </Grid>
     </Grid>
   );
